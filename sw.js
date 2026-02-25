26ea63a029a0407106b7544a01b6bec9de516298
quizmuhammad-iq <muhammad.sabbar.alsalmany@gmail.com>
2026-02-25 22:07:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,20 @@
-const CACHE_NAME = 'quizzes-v1';
+const CACHE_NAME = 'quizzes-v2';
 const STATIC_FILES = [
-  '/',
-  '/index.html',
-  '/quiz.html',
-  '/manifest.json',
-  '/icon.jpg'
+  './',
+  'index.html',
+  'quiz.html',
+  'manifest.json',
+  'icon.jpg'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_FILES))
   );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      return cached || fetch(e.request).then(res => {
-        if (res.ok) {
-          const clone = res.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-        }
-        return res;
-      }).catch(() => cached);
-    })
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
